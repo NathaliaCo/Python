cc036fb23b6919d86596d556335a3bf40a2faf6b
NathaliaCo <115585344+NathaliaCo@users.noreply.github.com>
2022-11-03 18:37:27 -0500

    Create Ejercicio2.py
    Ejercicio 2

---
+++ Taller introducción/Ejercicio2.py
@@ -0,0 +1,6 @@
+#ejercicio 2
+x=float(input("\n \t Ingresar el precio del producto: "))
+iva=0.21
+operacion_1=x*iva
+operacion_2=x+operacion_1
+print("\n\t El precio final con IVA es de:",operacion_2)
