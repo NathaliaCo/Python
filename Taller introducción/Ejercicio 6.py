9f43a440e60a6a99b486d12114042ef5329c341c
NathaliaCo <115585344+NathaliaCo@users.noreply.github.com>
2022-11-03 18:39:54 -0500

    Create Ejercicio 6.py
    Ejercicio 6

---
+++ Taller introducción/Ejercicio 6.py
@@ -0,0 +1,23 @@
+# ejercicio 6
+# área circulo
+		
+pi=3.1416
+radio= 6
+areaCirculo=pi*(radio**2)
+		
+print("El área del circulo es: ", areaCirculo);
+		
+#área triangulo
+		
+base= 3
+altura= 4
+areaTriangulo= int(base*altura/2)
+		
+print("El área del triángulo es: ",areaTriangulo)
+		
+#área cuadrado
+		
+lado= 2
+areaCuadrado= int(lado*lado)
+		
+print("El área del cuadrado es: ",areaCuadrado)
