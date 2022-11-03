2877c54ccafca129e4e7f18df40da1660fedaa4e
NathaliaCo <115585344+NathaliaCo@users.noreply.github.com>
2022-11-03 18:38:50 -0500

    Create Ejercicio 4.py
    Ejercicio 4

---
+++ Taller introducción/Ejercicio 4.py
@@ -0,0 +1,77 @@
+# ejercicio 4
+x=int(input("Ingrese el valor de x entero: "))
+y=int(input("Ingrese el valor de y entero: "))
+n=float(input("Ingrese el valor de n decimal: "))
+m=float(input("Ingrese el valor de m decimal: "))
+
+print("El valor de x es: ",x)
+print("El valor de y es: ",y)
+print("El valor de n es: ",n)
+print("El valor de m es: ",m)
+
+# operaciones x y y
+suma= x+y
+print("El resultado de la suma de",x, "y", y,"es: ",suma)
+
+resta= x-y
+print("El resultado de la resta de",x, "y", y,"es: ",resta)
+
+producto= x*y
+print("El resultado del producto de",x, "y", y,"es: ",producto)
+
+cociente= x/y
+print("El resultado del cociente de",x, "y", y,"es: ",cociente)
+
+residuo= x%y
+print("El resultado del residuo de",x, "y", y,"es: ",residuo)
+
+# operaciones n y m
+suma2= n+m
+print("El resultado de la suma de",n, "y", m,"es: ",suma2)
+
+resta2= n-m
+print("El resultado de la resta de",n, "y", m,"es: ",resta2)
+
+producto2= n*m
+print("El resultado del producto de",n, "y", m,"es: ",producto2)
+
+cociente2= n/m
+print("El resultado del cociente de",n, "y", m,"es: ",cociente2)
+
+residuo2= n%m
+print("El resultado del residuo de",n, "y", m,"es: ",residuo2)
+
+# operacion x y n
+suma3=x+n
+print("El resultado de la suma de",x, "y", n,"es: ",suma3)
+
+# operaciones y y m
+cociente3=y/m
+print("El resultado del cociente de",y, "y", m,"es: ",cociente3)
+
+residuo3=y%m
+print("El resultado del residuo de",y, "y", m,"es: ",residuo3)
+
+# operacion doble de cada variable
+
+doble1=x*2
+print("El doble de ",x, "es: ",doble1)
+
+doble2=y*2
+print("El doble de ",y, "es: ",doble2)
+
+doble3=n*2
+print("El doble de ",n, "es: ",doble3)
+
+doble4=m*2
+print("El doble de ",m, "es: ",doble4)
+
+# operacion suma de todas las variables 
+
+sumaTotal=x+y+n+m
+print("El resultado de la suma de todas las variables es: ", sumaTotal)
+
+# operacion producto de todas las variables
+
+productoTotal=x*y*n*m
+print("El resultado del producto de todas las variables es: ", productoTotal)
