e09ccba4b5cffa361ed7afc53dd3d09c9ea39246
NathaliaCo <115585344+NathaliaCo@users.noreply.github.com>
2022-11-10 18:03:36 -0500

    Create Ejercicio 2.py
    Ejercicio 2

---
+++ Quiz/Ejercicio 2.py
@@ -0,0 +1,15 @@
+x="ciclos"
+xsignificado="Te permite repetir una o varias instrucciones cuantas veces lo necesitemos."
+y="Listas"
+ysignificado="Son un tipo contenedor, compuesto, que se usan para almacenar conjuntos de elementos relacionados del mismo tipo o de tipos distintos."
+s="int"
+ssignificado="Función que convierte un valor especifico en un número entero."
+u="Diccionario"
+usignificado="Es una estructura de datos para trabajar con colecciones de datos almacenados en pares de claves/valores."
+o="Float"
+osignificado="Función que convierte un valor especifico en un número decimal."
+print("\n\t La primera palabra es,",x,",y su significado es el siguiente:",xsignificado)
+print("\n\t La primera palabra es,",y,",y su significado es el siguiente:",ysignificado)
+print("\n\t La primera palabra es,",s,",y su significado es el siguiente:",ssignificado)
+print("\n\t La primera palabra es,",u,",y su significado es el siguiente:",usignificado)
+print("\n\t La primera palabra es,",o,",y su significado es el siguiente:",osignificado)
