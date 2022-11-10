011e74c0a7cebe9d34eddc439f64a3c31f1adc0c
NathaliaCo <115585344+NathaliaCo@users.noreply.github.com>
2022-11-10 18:02:31 -0500

    Create Ejercicio 1.py
    Ejercicio 1

---
+++ Quiz/Ejercicio 1.py
@@ -0,0 +1,13 @@
+diccionario ={'Amazonas':'Bolivia','Misisipi':'Estados Unidos','Ganges':'Nepal','Nilo':'Egipto','Volga':'Rusia'}
+
+for x in diccionario:
+    print ("El río "+x+" pasa por "+diccionario[x])
+
+print ("-------------------------------------") 
+for x in diccionario:
+  print (x)
+
+print ("-------------------------------------") 
+
+for x in diccionario:
+  print (diccionario[x])
